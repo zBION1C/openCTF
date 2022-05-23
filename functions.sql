f45343561aefb5b7b705a6ae3b2b527bc6a64217
zBION1C <57861846+zBION1C@users.noreply.github.com>
2022-05-23 15:59:48 +0200

    Add files via upload

---
+++ functions.sql
@@ -0,0 +1,139 @@
+DROP FUNCTION UserPoints;
+DROP FUNCTION UserPointsCat;
+DROP FUNCTION UserResolvedCtfs;
+DROP FUNCTION UserResolvedCtfsCat;
+DROP FUNCTION FirstResolver;
+DROP FUNCTION NumberResolver;
+DROP FUNCTION NumberAttempts;
+
+DELIMITER $$
+CREATE FUNCTION UserPoints(name VARCHAR(50))
+RETURNS INTEGER DETERMINISTIC
+BEGIN
+	DECLARE points INTEGER;
+
+	SELECT sum(CTF.difficolta) INTO points 
+	FROM Risolte, CTF
+	WHERE Risolte.utente = name AND
+		  Risolte.ctf = CTF.id;
+	
+	IF ISNULL(points) THEN
+		RETURN 0;
+	ELSE
+		RETURN points;
+	END IF;
+END
+$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE FUNCTION UserPointsCat(name VARCHAR(50), category ENUM("Binary Exploitation", "Web Exploitation", "Reverse Engineering", "Forensic"))
+RETURNS INTEGER DETERMINISTNumberAttemptsIC
+BEGIN
+	DECLARE points INTEGER;
+	
+	SELECT sum(CTF.difficolta) INTO points 
+	FROM Risolte, CTF
+	WHERE Risolte.utente = name AND
+		  Risolte.ctf = CTF.id AND
+		  CTF.categoria = category;
+	
+	IF ISNULL(points) THEN
+		RETURN 0;
+	ELSE
+		RETURN points;
+	END IF;
+END
+$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE FUNCTION UserResolvedCtfs(name VARCHAR(50))
+RETURNS INTEGER DETERMINISTIC
+BEGIN
+	DECLARE n INTEGER;
+	
+	SELECT count(*) INTO n 
+	FROM Risolte, CTF
+	WHERE Risolte.utente = name AND
+		  Risolte.ctf = CTF.id;
+	
+	IF ISNULL(n) THEN
+		RETURN 0;
+	ELSE
+		RETURN n;
+	END IF;
+END
+$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE FUNCTION UserResolvedCtfsCat(name VARCHAR(50), category ENUM("Binary Exploitation", "Web Exploitation", "Reverse Engineering", "Forensic"))
+RETURNS INTEGER DETERMINISTIC
+BEGIN
+	DECLARE n INTEGER;
+	
+	SELECT count(*) INTO n 
+	FROM Risolte, CTF
+	WHERE Risolte.utente = name AND
+		  Risolte.ctf = CTF.id AND
+		  CTF.categoria = category;
+	
+	IF ISNULL(n) THEN
+		RETURN 0;
+	ELSE
+		RETURN n;
+	END IF;
+END
+$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE FUNCTION FirstResolver(ctf INTEGER)
+RETURNS VARCHAR(50) DETERMINISTIC
+BEGIN
+	DECLARE user VARCHAR(50);
+	
+	SELECT Risolte.utente INTO user 
+	FROM  CTF, Risolte
+	WHERE CTF.id = ctf AND
+	      Risolte.ctf = CTF.id
+	ORDER BY Risolte.ts
+	LIMIT 1; 
+	
+	RETURN user;
+END
+$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE FUNCTION NumberResolver(ctf INTEGER)
+RETURNS INTEGER DETERMINISTIC
+BEGIN
+	DECLARE n INTEGER;
+	
+	SELECT count(*) INTO n 
+	FROM  CTF, Risolte
+	WHERE CTF.id = ctf AND
+	      Risolte.ctf = CTF.id;
+	
+	RETURN n;
+END
+$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE FUNCTION NumberAttempts(ctf INTEGER)
+RETURNS INTEGER DETERMINISTIC
+BEGIN
+	DECLARE n INTEGER;
+	
+	SELECT count(*) INTO n 
+	FROM  CTF, Prova
+	WHERE CTF.id = ctf AND
+	      Prova.ctf = CTF.id;
+	
+	RETURN n;
+END
+$$
+DELIMITER ;
