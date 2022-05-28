77bbaca652cba35ae6cd908cb6ee498bd2c6d995
zBION1C <57861846+zBION1C@users.noreply.github.com>
2022-05-28 11:31:12 +0200

    Add files via upload

--- functions.sql
+++ functions.sql
@@ -5,6 +5,9 @@ DROP FUNCTION UserResolvedCtfsCat;
 DROP FUNCTION FirstResolver;
 DROP FUNCTION NumberResolver;
 DROP FUNCTION NumberAttempts;
+DROP FUNCTION UserAttemptedCtf;
+DROP FUNCTION NumberFirstResolver;
+DROP FUNCTION AlreadyResolved;
 
 DELIMITER $$
 CREATE FUNCTION UserPoints(name VARCHAR(50))
@@ -28,7 +31,7 @@ DELIMITER ;
 
 DELIMITER $$
 CREATE FUNCTION UserPointsCat(name VARCHAR(50), category ENUM("Binary Exploitation", "Web Exploitation", "Reverse Engineering", "Forensic"))
-RETURNS INTEGER DETERMINISTNumberAttemptsIC
+RETURNS INTEGER DETERMINISTIC
 BEGIN
 	DECLARE points INTEGER;
 	
@@ -89,6 +92,26 @@ $$
 DELIMITER ;
 
 DELIMITER $$
+CREATE FUNCTION UserAttemptedCtf(name VARCHAR(50))
+RETURNS INTEGER DETERMINISTIC
+BEGIN
+	DECLARE n INTEGER;
+	
+	SELECT count(*) INTO n 
+	FROM Prova, CTF
+	WHERE Prova.utente = name AND
+		  Prova.ctf = CTF.id;
+	
+	IF ISNULL(n) THEN
+		RETURN 0;
+	ELSE
+		RETURN n;
+	END IF;
+END
+$$
+DELIMITER ;
+
+DELIMITER $$
 CREATE FUNCTION FirstResolver(ctf INTEGER)
 RETURNS VARCHAR(50) DETERMINISTIC
 BEGIN
@@ -102,6 +125,24 @@ BEGIN
 	LIMIT 1; 
 	
 	RETURN user;
+END
+$$
+DELIMITER ;
+
+
+DELIMITER $$
+CREATE FUNCTION NumberFirstResolver(name VARCHAR(50))
+RETURNS INTEGER DETERMINISTIC
+BEGIN
+	DECLARE n INTEGER;
+	
+	SELECT count(*) INTO n
+	FROM  CTF, Risolte
+	WHERE Risolte.utente = name AND
+	      Risolte.ctf = CTF.id AND
+	      FirstResolver(CTF.id) = name;
+	
+	RETURN n;
 END
 $$
 DELIMITER ;
@@ -134,6 +175,27 @@ BEGIN
 	      Prova.ctf = CTF.id;
 	
 	RETURN n;
+END
+$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE FUNCTION AlreadyResolved(ctf INTEGER, utente VARCHAR(50))
+RETURNS BOOLEAN DETERMINISTIC
+BEGIN
+	DECLARE n INTEGER;
+	
+	SELECT CTF.id INTO n
+	FROM  CTF, Risolte
+	WHERE CTF.id = ctf AND
+	      Risolte.ctf = CTF.id AND
+	      Risolte.utente = utente;
+
+	IF ISNULL(n) THEN
+		RETURN FALSE;
+	ELSE
+		RETURN TRUE;
+	END IF;
 END
 $$
 DELIMITER ;
